7b253a36d185e66a4b07a847ba358e28eee93a86
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-06 18:46:33 +0700

    enable dark mode, set colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light': '#f9f9f9',
+        'dark': '#1c1917',
+        'component': '#fff',
+        'component-dark': '#3f3f46',
+        'primary': '',
+        'danger': '' 
+      }
+    },
   },
   plugins: [],
 }
