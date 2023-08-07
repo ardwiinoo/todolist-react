6ff323ad76ef02101ef9213c7ab97498b35a2bdc
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-07 17:05:06 +0700

    create header and functional dark mode toggle

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -1,7 +1,29 @@
-export const AppContainer = () => {
+import { observer } from 'mobx-react';
+import { ThemeToggle } from './theme-toggle';
+import Logo from '../logo.svg'
+import { BaseText } from './base-text';
+import { useStore } from '../stores';
+import { useEffect } from 'react'
+
+export const AppContainer = observer(() => {
+    const store = useStore()
+
+    useEffect(() => {
+        document.body.setAttribute('data-mode', store.theme.themeMode)
+    }, [store.theme.themeMode])
+
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        <div className='max-w-screen-md mx-auto p-3'>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                    <img src={Logo} alt='logo'></img>
+                    <div className="text-xl text-primary">
+                        TodoList 
+                    </div>
+                </div>
+                <ThemeToggle></ThemeToggle>
+            </div>
+            <BaseText>Hello banggg</BaseText>
+        </div>
     )
-}
+})
