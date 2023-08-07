8a479d888b31075c59f24bba365f1b72d15dc089
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-07 21:12:31 +0700

    display listt all tasks and all completed tasks

---
+++ src/components/task-list.tsx
@@ -0,0 +1,26 @@
+import { observer } from "mobx-react"
+import { TaskItem } from "./task-item"
+import { useStore } from "../stores"
+import { BaseText } from "./base-text"
+
+export const TaskList = observer(() => {
+    const store = useStore()
+    
+    return (
+        <div className="mt-6">
+            {
+                store.task.tasks.map((task) => (
+                    <TaskItem key={task.id} task={task}></TaskItem>
+                ))
+            }
+            <BaseText className="p-3">
+                Completed ({store.task.completedTasks.length})
+            </BaseText>
+            {
+                store.task.completedTasks.map((task) => (
+                    <TaskItem key={task.id} task={task}></TaskItem>
+                ))
+            }
+        </div>
+    )
+})
